a823d8bd40eff185e6cb388f9e4bd7d5ef1cded3
c-bata <contact@c-bata.link>
2021-04-08 01:50:24 +0900

    Fix lint errors

--- integration_tests.py
+++ integration_tests.py
@@ -8,31 +8,38 @@ from pyppeteer import launch
 from pyppeteer.browser import Browser
 from wsgiref.simple_server import make_server
 
-
 host = "127.0.0.1"
 port = 8080
 
 
-async def take_screenshot(browser: Browser, url: str, path: str):
+async def take_screenshot(
+    browser: Browser, url: str, path: str, sleep_sec: int = 5
+) -> None:
     page = await browser.newPage()
-    await page.setViewport({'width': 1200, 'height': 3000})
+    await page.setViewport({"width": 1200, "height": 3000})
     await page.goto(url)
-    time.sleep(5)
-    await page.screenshot({'path': path})
+    time.sleep(sleep_sec)
+    await page.screenshot({"path": path})
 
 
-async def integration_test_main():
+async def integration_test_main() -> None:
     browser = await launch()
-    await take_screenshot(browser, f'http://{host}:{port}/dashboard/', 'study_list.png')
-    await take_screenshot(browser, f'http://{host}:{port}/dashboard/studies/0', 'study_detail_0.png')
-    await take_screenshot(browser, f'http://{host}:{port}/dashboard/studies/1', 'study_detail_1.png')
+    await take_screenshot(
+        browser, f"http://{host}:{port}/dashboard/", "study_list.png", sleep_sec=1
+    )
+    await take_screenshot(
+        browser, f"http://{host}:{port}/dashboard/studies/0", "study_detail_0.png"
+    )
+    await take_screenshot(
+        browser, f"http://{host}:{port}/dashboard/studies/1", "study_detail_1.png"
+    )
     await browser.close()
 
 
 def run_optuna() -> optuna.storages.InMemoryStorage:
     storage = optuna.storages.InMemoryStorage()
 
-    # study 0
+    # study (study_id=0)
     study0 = optuna.create_study(study_name="example-0", storage=storage)
 
     def objective(trial: optuna.Trial) -> float:
@@ -42,12 +49,12 @@ def run_optuna() -> optuna.storages.InMemoryStorage:
 
     study0.optimize(objective, n_trials=100)
 
-    # study 1 - no trials
+    # study (study_id=1) with no trials
     optuna.create_study(study_name="example-1", storage=storage)
     return storage
 
 
-def main():
+def main() -> None:
     storage = run_optuna()
     app = create_app(storage)
 
@@ -62,5 +69,5 @@ def main():
     thread.join()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
