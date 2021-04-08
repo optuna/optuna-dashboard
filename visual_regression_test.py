7348c760d8abb3bbe2e5548731830496ae0364af
c-bata <contact@c-bata.link>
2021-04-08 17:04:52 +0900

    Fix lint errors

--- visual_regression_test.py
+++ visual_regression_test.py
@@ -6,6 +6,7 @@ import os
 
 from optuna_dashboard.app import create_app
 from pyppeteer import launch
+from typing import List, Tuple
 from wsgiref.simple_server import make_server
 
 host = "127.0.0.1"
@@ -19,29 +20,35 @@ def create_optuna_storage() -> optuna.storages.InMemoryStorage:
     # Single-objective study
     study = optuna.create_study(study_name="single-objective", storage=storage)
 
-    def objective(trial: optuna.Trial) -> float:
+    def objective_single(trial: optuna.Trial) -> float:
         x1 = trial.suggest_float("x1", 0, 10)
         x2 = trial.suggest_float("x2", 0, 10)
         return (x1 - 2) ** 2 + (x2 - 5) ** 2
 
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective_single, n_trials=100)
 
     # Multi-objective study
-    study = optuna.create_study(study_name="multi-objective", storage=storage, directions=["minimize", "minimize"])
+    study = optuna.create_study(
+        study_name="multi-objective",
+        storage=storage,
+        directions=["minimize", "minimize"],
+    )
 
-    def objective(trial):
+    def objective_multi(trial: optuna.Trial) -> Tuple[float, float]:
         x = trial.suggest_float("x", 0, 5)
         y = trial.suggest_float("y", 0, 3)
         v0 = 4 * x ** 2 + 4 * y ** 2
         v1 = (x - 5) ** 2 + (y - 5) ** 2
         return v0, v1
 
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective_multi, n_trials=50)
 
     # Pruning with no intermediate values
-    study = optuna.create_study(study_name="binh-korn-function-with-constraints", storage=storage)
+    study = optuna.create_study(
+        study_name="binh-korn-function-with-constraints", storage=storage
+    )
 
-    def objective(trial):
+    def objective_prune_with_no_trials(trial: optuna.Trial) -> float:
         x = trial.suggest_float("x", -15, 30)
         y = trial.suggest_float("y", -15, 30)
         v = x ** 2 + y ** 2
@@ -49,14 +56,14 @@ def create_optuna_storage() -> optuna.storages.InMemoryStorage:
             raise optuna.TrialPruned()
         return v
 
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective_prune_with_no_trials, n_trials=100)
 
     # No trials
     optuna.create_study(study_name="no trials", storage=storage)
     return storage
 
 
-async def take_screenshots(study_ids) -> None:
+async def take_screenshots(study_ids: List[int]) -> None:
     browser = await launch()
     page = await browser.newPage()
     await page.setViewport({"width": 1200, "height": 3000})
@@ -68,7 +75,9 @@ async def take_screenshots(study_ids) -> None:
     for study_id in study_ids:
         await page.goto(f"http://{host}:{port}/dashboard/studies/{study_id}")
         time.sleep(5)
-        await page.screenshot({"path": os.path.join(output_dir, f"study-{study_id}.png")})
+        await page.screenshot(
+            {"path": os.path.join(output_dir, f"study-{study_id}.png")}
+        )
     await browser.close()
 
 
