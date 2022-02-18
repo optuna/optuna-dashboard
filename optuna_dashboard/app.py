ae2190c1c6b0684be1ac5254b00b2f0e81e65d80
c-bata <contact@c-bata.link>
2022-02-18 15:25:35 +0900

    Fix lint errors

--- optuna_dashboard/app.py
+++ optuna_dashboard/app.py
@@ -8,7 +8,8 @@ from ._app import create_app as _create_app
 
 def create_app(storage: BaseStorage) -> Bottle:
     warnings.warn(
-        "This function will be removed in the future. Please use optuna_dashboard.run_server() instead.",
+        "This function will be removed in the future."
+        " Please use optuna_dashboard.run_server() instead.",
         DeprecationWarning,
     )
     return _create_app(storage)
