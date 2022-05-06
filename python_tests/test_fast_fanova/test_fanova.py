d85016f3126a9c0b69339b72d63ec172cc651d1d
c-bata <contact@c-bata.link>
2022-05-07 03:24:49 +0900

    Add fANOVA tests

---
+++ python_tests/test_fast_fanova/test_fanova.py
@@ -0,0 +1,168 @@
+from typing import Tuple
+
+from optuna import create_study, create_trial
+from optuna import Trial
+from optuna.samplers import RandomSampler
+import pytest
+
+try:
+    from optuna_dashboard._fast_fanova import FanovaImportanceEvaluator
+    from optuna.distributions import FloatDistribution
+except:
+    FanovaImportanceEvaluator = None  # type: ignore
+    FloatDistribution = None  # type: ignore
+
+
+if FanovaImportanceEvaluator is None:
+    pytest.skip("Cython module hasn't been built yet.", allow_module_level=True)
+
+
+def objective(trial: Trial) -> float:
+    x1 = trial.suggest_float("x1", 0.1, 3)
+    x2 = trial.suggest_float("x2", 0.1, 3, log=True)
+    x3 = trial.suggest_float("x3", 2, 4, log=True)
+    return x1 + x2 * x3
+
+
+def multi_objective_function(trial: Trial) -> Tuple[float, float]:
+    x1 = trial.suggest_float("x1", 0.1, 3)
+    x2 = trial.suggest_float("x2", 0.1, 3, log=True)
+    x3 = trial.suggest_float("x3", 2, 4, log=True)
+    return x1, x2 * x3
+
+
+def test_fanova_importance_evaluator_n_trees() -> None:
+    # Assumes that `seed` can be fixed to reproduce identical results.
+    study = create_study(sampler=RandomSampler(seed=0))
+    study.optimize(objective, n_trials=3)
+
+    evaluator = FanovaImportanceEvaluator(n_trees=10, seed=0)
+    param_importance = evaluator.evaluate(study)
+
+    evaluator = FanovaImportanceEvaluator(n_trees=20, seed=0)
+    param_importance_different_n_trees = evaluator.evaluate(study)
+
+    assert param_importance != param_importance_different_n_trees
+
+
+def test_fanova_importance_evaluator_max_depth() -> None:
+    # Assumes that `seed` can be fixed to reproduce identical results.
+    study = create_study(sampler=RandomSampler(seed=0))
+    study.optimize(objective, n_trials=3)
+
+    evaluator = FanovaImportanceEvaluator(max_depth=1, seed=0)
+    param_importance = evaluator.evaluate(study)
+
+    evaluator = FanovaImportanceEvaluator(max_depth=2, seed=0)
+    param_importance_different_max_depth = evaluator.evaluate(study)
+
+    assert param_importance != param_importance_different_max_depth
+
+
+def test_fanova_importance_evaluator_seed() -> None:
+    study = create_study(sampler=RandomSampler(seed=0))
+    study.optimize(objective, n_trials=3)
+
+    evaluator = FanovaImportanceEvaluator(seed=2)
+    param_importance = evaluator.evaluate(study)
+
+    evaluator = FanovaImportanceEvaluator(seed=2)
+    param_importance_same_seed = evaluator.evaluate(study)
+    assert param_importance == param_importance_same_seed
+
+    evaluator = FanovaImportanceEvaluator(seed=3)
+    param_importance_different_seed = evaluator.evaluate(study)
+    assert param_importance != param_importance_different_seed
+
+
+def test_fanova_importance_evaluator_with_target() -> None:
+    # Assumes that `seed` can be fixed to reproduce identical results.
+
+    study = create_study(sampler=RandomSampler(seed=0))
+    study.optimize(objective, n_trials=3)
+
+    evaluator = FanovaImportanceEvaluator(seed=0)
+    param_importance = evaluator.evaluate(study)
+    param_importance_with_target = evaluator.evaluate(
+        study,
+        target=lambda t: t.params["x1"] + t.params["x2"],
+    )
+
+    assert param_importance != param_importance_with_target
+
+
+@pytest.mark.skipif(FloatDistribution is None)
+@pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
+def test_fanova_importance_evaluator_with_infinite(inf_value: float) -> None:
+    # The test ensures that trials with infinite values are ignored to calculate importance scores.
+    n_trial = 10
+    seed = 13
+
+    # Importance scores are calculated without a trial with an inf value.
+    study = create_study(sampler=RandomSampler(seed=seed))
+    study.optimize(objective, n_trials=n_trial)
+
+    evaluator = FanovaImportanceEvaluator(seed=seed)
+    param_importance_without_inf = evaluator.evaluate(study)
+
+    # A trial with an inf value is added into the study manually.
+    study.add_trial(
+        create_trial(
+            value=inf_value,
+            params={"x1": 1.0, "x2": 1.0, "x3": 3.0},
+            distributions={
+                "x1": FloatDistribution(low=0.1, high=3),
+                "x2": FloatDistribution(low=0.1, high=3, log=True),
+                "x3": FloatDistribution(low=2, high=4, log=True),
+            },
+        )
+    )
+    # Importance scores are calculated with a trial with an inf value.
+    param_importance_with_inf = evaluator.evaluate(study)
+
+    # Obtained importance scores should be the same between with inf and without inf,
+    # because the last trial whose objective value is an inf is ignored.
+    assert param_importance_with_inf == param_importance_without_inf
+
+
+@pytest.mark.skipif(FloatDistribution is None)
+@pytest.mark.parametrize("target_idx", [0, 1])
+@pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
+def test_multi_objective_fanova_importance_evaluator_with_infinite(
+    target_idx: int, inf_value: float
+) -> None:
+    # The test ensures that trials with infinite values are ignored to calculate importance scores.
+    n_trial = 10
+    seed = 13
+
+    # Importance scores are calculated without a trial with an inf value.
+    study = create_study(
+        directions=["minimize", "minimize"], sampler=RandomSampler(seed=seed)
+    )
+    study.optimize(multi_objective_function, n_trials=n_trial)
+
+    evaluator = FanovaImportanceEvaluator(seed=seed)
+    param_importance_without_inf = evaluator.evaluate(
+        study, target=lambda t: t.values[target_idx]
+    )
+
+    # A trial with an inf value is added into the study manually.
+    study.add_trial(
+        create_trial(
+            values=[inf_value, inf_value],
+            params={"x1": 1.0, "x2": 1.0, "x3": 3.0},
+            distributions={
+                "x1": FloatDistribution(low=0.1, high=3),
+                "x2": FloatDistribution(low=0.1, high=3, log=True),
+                "x3": FloatDistribution(low=2, high=4, log=True),
+            },
+        )
+    )
+    # Importance scores are calculated with a trial with an inf value.
+    param_importance_with_inf = evaluator.evaluate(
+        study, target=lambda t: t.values[target_idx]
+    )
+
+    # Obtained importance scores should be the same between with inf and without inf,
+    # because the last trial whose objective value is an inf is ignored.
+    assert param_importance_with_inf == param_importance_without_inf
