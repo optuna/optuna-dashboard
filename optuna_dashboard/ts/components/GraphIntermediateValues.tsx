a1b80b4571c862b28a285860b8d73d10cce58eda
yoshinobc <t.980527t@gmail.com>
2022-05-09 14:33:51 +0900

    sorting in action.ts

--- optuna_dashboard/ts/components/GraphIntermediateValues.tsx
+++ optuna_dashboard/ts/components/GraphIntermediateValues.tsx
@@ -51,31 +51,24 @@ const plotIntermediateValue = (trials: Trial[], mode: string) => {
     return
   }
 
-  const sortedFilteredTrials = trials
-    .filter(
-      (t) =>
-        t.state === "Complete" ||
-        (t.state === "Pruned" && t.values && t.values.length > 0) ||
-        t.state == "Running"
-    )
-    .slice()
-    .sort((a: Trial, b: Trial) => a.number - b.number)
-  const plotData: Partial<plotly.PlotData>[] = sortedFilteredTrials.map(
-    (trial) => {
-      const values = trial.intermediate_values.filter(
-        (iv) => iv.value !== "inf"
-      )
-      return {
-        x: values.map((iv) => iv.step),
-        y: values.map((iv) => iv.value),
-        mode: "lines+markers",
-        type: "scatter",
-        name:
-          trial.state !== "Running"
-            ? `trial #${trial.number}`
-            : `trial #${trial.number} (running)`,
-      }
-    }
+  const FilteredTrials = trials.filter(
+    (t) =>
+      t.state === "Complete" ||
+      (t.state === "Pruned" && t.values && t.values.length > 0) ||
+      t.state == "Running"
   )
+  const plotData: Partial<plotly.PlotData>[] = FilteredTrials.map((trial) => {
+    const values = trial.intermediate_values.filter((iv) => iv.value !== "inf")
+    return {
+      x: values.map((iv) => iv.step),
+      y: values.map((iv) => iv.value),
+      mode: "lines+markers",
+      type: "scatter",
+      name:
+        trial.state !== "Running"
+          ? `trial #${trial.number}`
+          : `trial #${trial.number} (running)`,
+    }
+  })
   plotly.react(plotDomId, plotData, layout)
 }
