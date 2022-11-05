3d971a89b8b7e7c427405e59e477a8ad0977d17b
keisuke-umezawa <keisuke.umezawa@gmail.com>
2022-11-05 16:19:49 +0900

    Align graph parallel coordinate

--- optuna_dashboard/ts/components/GraphParallelCoordinate.tsx
+++ optuna_dashboard/ts/components/GraphParallelCoordinate.tsx
@@ -89,9 +89,9 @@ const plotCoordinate = (
   const layout: Partial<plotly.Layout> = {
     margin: {
       l: 70,
-      t: 100,
+      t: 50,
       r: 50,
-      b: 0,
+      b: 100,
     },
     template: mode === "dark" ? plotlyDarkTemplate : {},
   }
@@ -168,9 +168,19 @@ const plotCoordinate = (
   const plotData: Partial<plotly.PlotData>[] = [
     {
       type: "parcoords",
-      // @ts-ignore
       dimensions: dimensions,
       labelangle: 30,
+      labelside: "bottom",
+      line: {
+        color: dimensions[0]["values"],
+        // @ts-ignore
+        colorscale: "Blues",
+        colorbar: {
+          title: "Objective value",
+        },
+        showscale: true,
+        reversescale: study.directions[objectiveId] === "maximize",
+      },
     },
   ]
 
