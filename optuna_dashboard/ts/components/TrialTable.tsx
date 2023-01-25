7d796acb2e8502daa45bfe6bea8f3a1ac289ed5f
keisuke-umezawa <keisuke.umezawa@gmail.com>
2023-01-25 21:43:30 +0900

    Fix regex for float numbers

--- optuna_dashboard/ts/components/TrialTable.tsx
+++ optuna_dashboard/ts/components/TrialTable.tsx
@@ -361,7 +361,7 @@ export const TrialTable: FC<{
                           inputProps={{
                             inputMode: "numeric",
                             pattern:
-                              "[+-]?([0-9]+(.[0-9]*)?|.[0-9]+)([eE][+-]?[0-9]+)?",
+                              "[-+]?[0-9]*.?[0-9]+([eE][-+]?[0-9]+)?",
                             title: "Please input a float number",
                           }}
                           inputRef={ref}
