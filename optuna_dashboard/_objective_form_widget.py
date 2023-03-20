9c477cb3afade0c8423b9fa82f762695c5d37674
Kenshin Abe <abe.kenshin@gmail.com>
2023-03-20 15:39:18 +0900

    Add validation of `user_attr_key`

--- optuna_dashboard/_objective_form_widget.py
+++ optuna_dashboard/_objective_form_widget.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import json
 from typing import TYPE_CHECKING
 from typing import Union
+import warnings
 
 import optuna
 
@@ -132,6 +133,8 @@ def register_objective_form_widgets(
 ) -> None:
     if len(study.directions) != len(widgets):
         raise ValueError("The length of actions must be the same with the number of objectives.")
+    if any(w.user_attr_key is not None for w in widgets):
+        warnings.warn("`user_attr_key` specified, but it will not be used.")
     widget_dicts = [w.to_dict() for w in widgets]
     study._storage.set_study_system_attr(study._study_id, SYSTEM_ATTR_KEY, widget_dicts)
     study._storage.set_study_system_attr(study._study_id, SYSTEM_ATTR_OUTPUT_TYPE_KEY, "objective")
@@ -140,6 +143,10 @@ def register_objective_form_widgets(
 def register_user_attr_form_widgets(
     study: optuna.Study, widgets: list[ObjectiveFormWidget]
 ) -> None:
+    if any(w.user_attr_key is None for w in widgets):
+        raise ValueError("`user_attr_key` is not specified.")
+    if len(widgets) != len(set(w.user_attr_key for w in widgets)):
+        raise ValueError("`user_attr_key` must be unique for each widget.")
     widget_dicts = [w.to_dict() for w in widgets]
     study._storage.set_study_system_attr(study._study_id, SYSTEM_ATTR_KEY, widget_dicts)
     study._storage.set_study_system_attr(study._study_id, SYSTEM_ATTR_OUTPUT_TYPE_KEY, "user_attr")
