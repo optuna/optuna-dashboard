5bded1344b77348cb45ac50e1486b3b2a9907262
c-bata <contact@c-bata.link>
2023-04-11 11:39:58 +0900

    Support Optuna metric_names

--- optuna_dashboard/_named_objectives.py
+++ optuna_dashboard/_named_objectives.py
@@ -2,9 +2,14 @@ from __future__ import annotations
 
 from typing import Any
 from typing import Optional
+import warnings
 
 import optuna
 
+
+# Should be equivalent to `optuna.study.study._SYSTEM_ATTR_METRIC_NAMES`.
+# See https://github.com/optuna/optuna/pull/4383 for details.
+SYSTEM_ATTR_METRIC_NAMES = "study:metric_names"
 
 SYSTEM_ATTR_NAME = "dashboard:objective_names"
 
@@ -22,6 +27,17 @@ def set_objective_names(study: optuna.Study, names: list[str]) -> None:
           study = optuna.create_study(directions=["minimize", "minimize"])
           set_objective_names(study, ["val_loss", "flops"])
     """
+
+    if hasattr(study, "set_metric_names"):
+        # TODO(c-bata): List this warning on errors.rst in docs.
+        warnings.warn(
+            "`set_objective_names()` function is deprecated."
+            "Please use `study.set_metric_names([...])` instead.",
+            category=FutureWarning,
+        )
+        study.set_metric_names(names)
+        return
+
     storage = study._storage
     study_id = study._study_id
 
@@ -32,4 +48,6 @@ def set_objective_names(study: optuna.Study, names: list[str]) -> None:
 
 
 def get_objective_names(system_attrs: dict[str, Any]) -> Optional[list[str]]:
+    if SYSTEM_ATTR_METRIC_NAMES in system_attrs:
+        return system_attrs[SYSTEM_ATTR_METRIC_NAMES]
     return system_attrs.get(SYSTEM_ATTR_NAME)
---
+++ python_tests/test_metric_names.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import unittest
+
+import optuna
+from optuna.version import __version__ as optuna_ver
+from optuna_dashboard._named_objectives import get_objective_names
+from packaging import version
+
+
+class MetricNamesTestCase(unittest.TestCase):
+    @unittest.skipIf(
+        version.parse(optuna_ver) < version.Version("3.2.0.dev"),
+        "study.set_metric_names() is not implemented yet",
+    )
+    def test_get_metric_names(self) -> None:
+        study = optuna.create_study(directions=["minimize", "minimize"])
+        study.set_metric_names(["val_loss", "flops"])
+
+        study_system_attrs = study._storage.get_study_system_attrs(study._study_id)
+        metric_names = get_objective_names(study_system_attrs)
+
+        assert metric_names == ["val_loss", "flops"]
