031a1e103a499349218e396faccd94a2edd5a6c6
Kenshin Abe <abe.kenshin@gmail.com>
2023-04-12 16:34:38 +0900

    Fix type

--- optuna_dashboard/ts/apiClient.ts
+++ optuna_dashboard/ts/apiClient.ts
@@ -67,8 +67,7 @@ interface StudyDetailResponse {
   has_intermediate_values: boolean
   note: Note
   objective_names?: string[]
-  objective_form_widgets?: ObjectiveFormWidget[]
-  form_widgets_output_type?: string
+  objective_form_widgets?: FormWidgets
 }
 
 export const getStudyDetailAPI = (
@@ -102,7 +101,6 @@ export const getStudyDetailAPI = (
         note: res.data.note,
         objective_names: res.data.objective_names,
         objective_form_widgets: res.data.objective_form_widgets,
-        form_widgets_output_type: res.data.form_widgets_output_type,
       }
     })
 }
