16a6265d64a6baa61ed7b685bdd77c14f1099fca
c-bata <contact@c-bata.link>
2023-05-10 09:51:27 +0900

    Fix a broken test

--- python_tests/test_serializers.py
+++ python_tests/test_serializers.py
@@ -10,17 +10,6 @@ class SerializeAttrsTestCase(TestCase):
         serialized = serialize_attrs({"bytes": b"This is a bytes object."})
         self.assertEqual(serialized[0]["value"], "<binary object>")
 
-    def test_serialize_string(self) -> None:
-        for length in [1000, 1024, 1100]:
-            with self.subTest(f"length: {length}"):
-                value = "a" * length
-                serialized = serialize_attrs(
-                    {
-                        "key": value,
-                    }
-                )
-                self.assertLessEqual(len(serialized[0]["value"]), 1024)
-
     def test_serialize_dict(self) -> None:
         serialized = serialize_attrs(
             {
