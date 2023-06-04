4f5cf5fd3db6de7ea02f553dfa5cd3e453a228ed
keisuke-umezawa <keisuke.umezawa@gmail.com>
2023-06-04 15:00:25 +0900

    Add settings for playwright-pytest

---
+++ e2e_tests/visual_regression_test.py
@@ -0,0 +1,21 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+    # create a locator
+    get_started = page.get_by_role("link", name="Get started")
+
+    # Expect an attribute "to be strictly equal" to the value.
+    expect(get_started).to_have_attribute("href", "/docs/intro")
+
+    # Click the get started link.
+    get_started.click()
+
+    # Expects the URL to contain intro.
+    expect(page).to_have_url(re.compile(".*intro"))
