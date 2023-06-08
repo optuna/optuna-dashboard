8c4205097cfafe72ae87a1206d6bd55ca518af32
c-bata <contact@c-bata.link>
2023-06-08 10:14:46 +0900

    Revert SupportsRead and use BinaryIO again

--- optuna_dashboard/artifact/boto3.py
+++ optuna_dashboard/artifact/boto3.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import io
-import shutil
 from typing import TYPE_CHECKING
 
 import boto3
@@ -11,11 +9,8 @@ from optuna_dashboard.artifact.exceptions import ArtifactNotFound
 
 if TYPE_CHECKING:
     from typing import BinaryIO
-    from typing import IO
     from typing import Optional
-    from typing import TypeGuard
 
-    from _typeshed import SupportsRead
     from mypy_boto3_s3 import S3Client
 
 
@@ -53,16 +48,8 @@ class Boto3Backend:
         assert body is not None
         return body  # type: ignore
 
-    def write(self, artifact_id: str, content_body: SupportsRead[bytes]) -> None:
-        if _is_file_like_obj(content_body):
-            self.client.upload_fileobj(content_body, self.bucket, artifact_id)
-            return
-
-        # Convert SupportsRead[bytes] to file-like object
-        buf = io.BytesIO()
-        shutil.copyfileobj(content_body, buf)
-        buf.seek(0)
-        self.client.upload_fileobj(buf, self.bucket, artifact_id)
+    def write(self, artifact_id: str, content_body: BinaryIO) -> None:
+        self.client.upload_fileobj(content_body, self.bucket, artifact_id)
 
     def remove(self, artifact_id: str) -> None:
         try:
@@ -77,15 +64,6 @@ def _is_not_found_error(e: ClientError) -> bool:
     error_code = e.response.get("Error", {}).get("Code")
     http_status_code = e.response.get("ResponseMetadata", {}).get("HTTPStatusCode")
     return error_code == "NoSuchKey" or http_status_code == 404
-
-
-def _is_file_like_obj(obj: SupportsRead[bytes]) -> TypeGuard[IO[bytes]]:
-    return (
-        isinstance(obj, io.TextIOBase)
-        or isinstance(obj, io.BufferedIOBase)
-        or isinstance(obj, io.RawIOBase)
-        or isinstance(obj, io.IOBase)
-    )
 
 
 if TYPE_CHECKING:
