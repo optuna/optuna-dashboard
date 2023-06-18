686d4b9121164ac179f3b5afdbe104df12fff43a
keisuke-umezawa <keisuke.umezawa@gmail.com>
2023-06-18 13:51:58 +0900

    Use parameterize test to generate screenshot of each page

--- e2e_tests/visual_regression_test.py
+++ e2e_tests/visual_regression_test.py
@@ -1,25 +1,29 @@
 import optuna
-from playwright.sync_api import Browser
+from playwright.sync_api import Page
+import pytest
 
 
+@pytest.mark.parametrize("study_id", range(10))
 def test_study_list(
+    study_id: int,
+    page: Page,
     dummy_storage: optuna.storages.InMemoryStorage,
-    server: None,
-    browser: Browser,
     port: int,
+    server: None,
 ) -> None:
+    page.set_viewport_size({"width": 1000, "height": 3000})
     url = f"http://localhost:{port}/dashboard/"
-    page = browser.new_page(viewport={"width": 1000, "height": 3000})
 
     summaries = optuna.get_all_study_summaries(dummy_storage)
     study_ids = {s._study_id: s.study_name for s in summaries}
-    for study_id, study_name in study_ids.items():
-        page.goto(url)
-        page.click(f"a[href='/dashboard/studies/{study_id}']")
 
-        element = page.query_selector(".MuiTypography-body1")
-        assert element is not None
+    study_name = study_ids[study_id]
+    page.goto(url)
+    page.click(f"a[href='/dashboard/studies/{study_id}']")
 
-        title = element.text_content()
-        assert title is not None
-        assert study_name in title
+    element = page.query_selector(".MuiTypography-body1")
+    assert element is not None
+
+    title = element.text_content()
+    assert title is not None
+    assert study_name in title
