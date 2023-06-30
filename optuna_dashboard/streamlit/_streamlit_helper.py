85bf158144229147a22262e9c302379d878eab10
cross32768 <suzukikaito14.ne.jp@gmail.com>
2023-06-30 15:47:39 +0900

    Fix import path

--- optuna_dashboard/streamlit/_streamlit_helper.py
+++ optuna_dashboard/streamlit/_streamlit_helper.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
 import optuna
+from typing import TYPE_CHECKING
+
 from optuna.trial import FrozenTrial
-from optuna_dashboard._form_widget import ChoiceWidgetJSON
-from optuna_dashboard._form_widget import get_form_widgets_json
-from optuna_dashboard._form_widget import SliderWidgetJSON
-from optuna_dashboard._form_widget import TextInputWidgetJSON
-from optuna_dashboard._note import get_note_from_system_attrs
+from .._form_widget import get_form_widgets_json
+from .._note import get_note_from_system_attrs
 import streamlit as st
+
+
+if TYPE_CHECKING:
+    from .._form_widget import TextInputWidgetJSON
+    from .._form_widget import ChoiceWidgetJSON
+    from .._form_widget import SliderWidgetJSON
 
 
 def render_trial_note(study: optuna.Study, trial: FrozenTrial) -> None:
--- streamlit/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._streamlit_helper import render_objective_form_widgets
-from ._streamlit_helper import render_trial_note
-from ._streamlit_helper import render_user_attr_form_widgets
