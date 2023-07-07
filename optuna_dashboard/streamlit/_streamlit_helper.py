87be6de36583827dc1616454fb6027c3f381b57c
cross32768 <suzukikaito14.ne.jp@gmail.com>
2023-07-07 09:52:24 +0900

    update some code structure

--- optuna_dashboard/streamlit/_streamlit_helper.py
+++ optuna_dashboard/streamlit/_streamlit_helper.py
@@ -1,18 +1,22 @@
 from __future__ import annotations
 
-import optuna
 from typing import TYPE_CHECKING
 
+import optuna
 from optuna.trial import FrozenTrial
+import streamlit as st
+
 from .._form_widget import get_form_widgets_json
 from .._note import get_note_from_system_attrs
-import streamlit as st
 
 
 if TYPE_CHECKING:
-    from .._form_widget import TextInputWidgetJSON
+    from typing import Sequence
+
     from .._form_widget import ChoiceWidgetJSON
     from .._form_widget import SliderWidgetJSON
+    from .._form_widget import TextInputWidgetJSON
+    from .._form_widget import UserAttrRefJSON
 
 
 def render_trial_note(study: optuna.Study, trial: FrozenTrial) -> None:
@@ -30,41 +34,45 @@ def _format_choice(choice: float, widget: ChoiceWidgetJSON) -> str:
     return widget["choices"][widget["values"].index(choice)]
 
 
+def _format_description(description: str | None) -> str:
+    return "" if description is None else description
+
+
 def _render_widgets(
-    widgets: list[ChoiceWidgetJSON | SliderWidgetJSON | TextInputWidgetJSON],
+    widgets: Sequence[ChoiceWidgetJSON | SliderWidgetJSON | TextInputWidgetJSON | UserAttrRefJSON],
+    trial: FrozenTrial,
 ) -> tuple[bool, list[str | float | None]]:
     values: list[str | float | None] = []
+
     with st.form("user_input", clear_on_submit=False):
         for widget in widgets:
-            if widget["description"] is None:
-                description = ""
-            else:
-                description = widget["description"]
-
             if widget["type"] == "choice":
                 value = st.radio(
-                    description,
+                    _format_description(widget["description"]),
                     widget["values"],
                     format_func=lambda choice, widget=widget: _format_choice(  # type: ignore
                         choice, widget
                     ),
                     horizontal=True,
                 )
-                values.append(value)
             elif widget["type"] == "slider":
                 # NOTE: It is difficult to reflect "labels".
                 value = st.slider(
-                    description,
+                    _format_description(widget["description"]),
                     min_value=widget["min"],
                     max_value=widget["max"],
                     step=widget["step"],
                 )
-                values.append(value)
             elif widget["type"] == "text":
                 # NOTE: Current implementation ignores "optional".
-                values.append(st.text_input(description))
+                value = st.text_input(_format_description(widget["description"]))
+            elif widget["type"] == "user_attr":
+                value = trial.user_attrs[widget["key"]]
             else:
-                raise ValueError("Widget type should be 'choice', 'slider' or 'text'.")
+                raise ValueError(
+                    "Widget type should be 'choice', 'slider', 'text', or 'user_attr'."
+                )
+            values.append(value)
         submitted = st.form_submit_button("Submit")
     return submitted, values
 
@@ -73,7 +81,6 @@ def render_user_attr_form_widgets(study: optuna.Study, trial: FrozenTrial) -> No
     """Render user input widgets to UI with streamlit.
 
     Submitted values to the forms are registered as each trial's user_attrs.
-    "type" of widgets should be "choice", "slider", or "text".
 
     Args:
         study: The optuna study object to get widget specification.
@@ -82,8 +89,6 @@ def render_user_attr_form_widgets(study: optuna.Study, trial: FrozenTrial) -> No
     Raises:
         ValueError: If No form widgets registered.
         ValueError: If 'output_type' of form widgets is not 'user_attr'.
-        ValueError: If any widget['type'] is not in ['choice', 'slider', 'text'].
-        ValueError: if any widget does not have 'user_attr_key'.
 
     """
 
@@ -95,19 +100,14 @@ def render_user_attr_form_widgets(study: optuna.Study, trial: FrozenTrial) -> No
         raise ValueError("'output_type' should be 'user_attr'.")
 
     widgets = form_widgets_dict["widgets"]
-    for widget in widgets:
-        if widget["type"] not in ["choice", "slider", "text"]:
-            raise ValueError("Widget type should be 'choice', 'slider' or 'text'.")
-        if widget["user_attr_key"] is None:  # type: ignore
-            raise ValueError("Widget should have 'user_attr_key'.")
-
-    submitted, values = _render_widgets(widget)  # type: ignore
+    submitted, values = _render_widgets(widgets, trial)
 
     if submitted:
         for widget, value in zip(widgets, values):
-            study._storage.set_trial_user_attr(
-                trial._trial_id, key=widget["user_attr_key"], value=value  # type: ignore
-            )
+            if "user_attr_key" in widget.keys():
+                study._storage.set_trial_user_attr(
+                    trial._trial_id, key=widget["user_attr_key"], value=value  # type: ignore
+                )
 
         st.success("Submitted!")
 
@@ -116,7 +116,7 @@ def render_objective_form_widgets(study: optuna.Study, trial: FrozenTrial) -> No
     """Render user input widgets to UI with streamlit.
 
     Submitted values to the forms are telled to optuna trial object.
-    "type" of widgets should be "choice" or "slider".
+    All submitted values should be float.
     Multiple widgets correspond to multi-objective optimization.
 
     Args:
@@ -126,23 +126,24 @@ def render_objective_form_widgets(study: optuna.Study, trial: FrozenTrial) -> No
     Raises:
         ValueError: If No form widgets registered.
         ValueError: If 'output_type' of form widgets is not 'objective'.
-        ValueError: If any widget['type'] is not in ['choice', 'slider'].
+        ValueError: If any submitted values cannot be converted to float.
     """
 
     form_widgets_dict = get_form_widgets_json(study.system_attrs)
     if form_widgets_dict is None:
         raise ValueError("No form widgets registered.")
 
-    if form_widgets_dict["output_type"] != "user_attr":
-        raise ValueError("'output_type' should be 'user_attr'.")
+    if form_widgets_dict["output_type"] != "objective":
+        raise ValueError("'output_type' should be 'objective'.")
 
-    widgets = form_widgets_dict["widgets"]
-    for widget in widgets:
-        if widget["type"] not in ["choice", "slider"]:
-            raise ValueError("Widget type should be 'choice' or 'slider'.")
-
-    submitted, values = _render_widgets(widgets)  # type: ignore
+    submitted, values = _render_widgets(form_widgets_dict["widgets"], trial)
 
     if submitted:
-        study.tell(trial.number, values)  # type: ignore
+        values_float = []
+        for value in values:
+            try:
+                values_float.append(float(value))  # type: ignore
+            except ValueError as e:
+                raise ValueError("All submitted values should be float.") from e
+        study.tell(trial.number, values_float)
         st.success("Submitted!")
