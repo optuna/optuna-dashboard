f8d7bf11d9ba11e299a30b07b5a45e5be576c7bc
Contramundum <contramundum53@outlook.com>
2023-08-14 12:00:10 +0900

    Revert evaluator

--- examples/preferential-optimization/evaluator.py
+++ examples/preferential-optimization/evaluator.py
@@ -12,7 +12,6 @@ from optuna_dashboard.artifact.file_system import FileSystemBackend
 import streamlit as st
 
 from optuna_dashboard.preferential import load_study
-from optuna_dashboard.
 
 
 STORAGE_URL = "sqlite:///st-example.db"
