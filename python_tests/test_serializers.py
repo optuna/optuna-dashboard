853dc7bb159cd1c21013a3032e5c42de08677f2f
c-bata <contact@c-bata.link>
2023-08-16 10:43:46 +0900

    Rewrite test_serializers in pytest-based style

--- python_tests/test_serializers.py
+++ python_tests/test_serializers.py
@@ -5,15 +5,15 @@ from unittest import TestCase
 from optuna_dashboard._serializer import serialize_attrs
 
 
-class SerializeAttrsTestCase(TestCase):
-    def test_serialize_bytes(self) -> None:
-        serialized = serialize_attrs({"bytes": b"This is a bytes object."})
-        self.assertEqual(serialized[0]["value"], "<binary object>")
+def test_serialize_bytes() -> None:
+    serialized = serialize_attrs({"bytes": b"This is a bytes object."})
+    assert serialized[0]["value"] == "<binary object>"
 
-    def test_serialize_dict(self) -> None:
-        serialized = serialize_attrs(
-            {
-                "key": {"foo": "bar"},
-            }
-        )
-        self.assertLessEqual(len(serialized), 1)
+
+def test_serialize_dict() -> None:
+    serialized = serialize_attrs(
+        {
+            "key": {"foo": "bar"},
+        }
+    )
+    assert len(serialized) <= 1
