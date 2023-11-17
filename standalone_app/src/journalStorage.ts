9930e1fe1fda2da1b85f00191cd79be93a1d407a
gen740 <gen740.f@gmail.com>
2023-11-17 17:57:24 +0900

    Add JournalOperation enum

--- standalone_app/src/journalStorage.ts
+++ standalone_app/src/journalStorage.ts
@@ -1,5 +1,18 @@
 import { SetterOrUpdater } from "recoil"
 
+enum JournalOperation {
+  CREATE_STUDY = 0,
+  DELETE_STUDY = 1,
+  SET_STUDY_USER_ATTR = 2,
+  SET_STUDY_SYSTEM_ATTR = 3,
+  CREATE_TRIAL = 4,
+  SET_TRIAL_PARAM = 5,
+  SET_TRIAL_STATE_VALUES = 6,
+  SET_TRIAL_INTERMEDIATE_VALUE = 7,
+  SET_TRIAL_USER_ATTR = 8,
+  SET_TRIAL_SYSTEM_ATTR = 9,
+}
+
 export const loadJournalStorage = (
   arrayBuffer: ArrayBuffer,
   setter: SetterOrUpdater<Study[]>
