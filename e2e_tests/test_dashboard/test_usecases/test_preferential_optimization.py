be2c3340e5730a71e511a893ba243292c8f0e5ff
keisuke-umezawa <keisuke.umezawa@gmail.com>
2024-01-08 13:37:54 +0900

    Clear inmemory chache when terminating test

--- e2e_tests/test_dashboard/test_usecases/test_preferential_optimization.py
+++ e2e_tests/test_dashboard/test_usecases/test_preferential_optimization.py
@@ -9,7 +9,6 @@ from playwright.sync_api import Page
 import pytest
 
 from ...test_server import make_test_server
-from ...utils import clear_inmemory_cache
 
 
 def make_test_storage() -> optuna.storages.InMemoryStorage:
@@ -44,7 +43,6 @@ def make_test_storage() -> optuna.storages.InMemoryStorage:
 
 @pytest.fixture
 def storage() -> optuna.storages.InMemoryStorage:
-    clear_inmemory_cache()
     storage = make_test_storage()
     return storage
 
