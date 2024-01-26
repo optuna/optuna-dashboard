27cdde3a1b82dde94d2f89fe2013daac70eb0cd4
Kenshin Abe <abe.kenshin@gmail.com>
2024-01-26 19:19:35 +0900

    Implement GraphRankBackend

--- optuna_dashboard/ts/components/GraphRank.tsx
+++ optuna_dashboard/ts/components/GraphRank.tsx
@@ -14,6 +14,8 @@ import {
 import { plotlyDarkTemplate } from "./PlotlyDarkMode"
 import { getAxisInfo, makeHovertext } from "../graphUtil"
 import { useMergedUnionSearchSpace } from "../searchSpace"
+import { getPlotAPI, PlotType } from "../apiClient"
+import { useBackendRender } from "../state"
 
 const plotDomId = "graph-rank"
 
@@ -30,6 +32,35 @@ interface RankPlotInfo {
 }
 
 export const GraphRank: FC<{
+  study: StudyDetail | null
+}> = ({ study = null }) => {
+  if (useBackendRender()) {
+    return <GraphRankBackend study={study} />
+  } else {
+    return <GraphRankFrontend study={study} />
+  }
+}
+
+const GraphRankBackend: FC<{
+  study: StudyDetail | null
+}> = ({ study = null }) => {
+  const studyId = study?.id
+  useEffect(() => {
+    if (studyId === undefined) {
+      return
+    }
+    getPlotAPI(studyId, PlotType.Rank)
+      .then(({ data, layout }) => {
+        plotly.react(plotDomId, data, layout)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [studyId])
+  return <Box id={plotDomId} sx={{ height: "450px" }} />
+}
+
+const GraphRankFrontend: FC<{
   study: StudyDetail | null
 }> = ({ study = null }) => {
   const theme = useTheme()
