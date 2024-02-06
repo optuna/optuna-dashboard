fe00b0285ef8bc5bd181fd68c2a7013fdb34fae8
Contramundum <contramundum53@outlook.com>
2024-02-06 21:33:27 +0900

    Fix overflow

--- optuna_dashboard/preferential/samplers/gp.py
+++ optuna_dashboard/preferential/samplers/gp.py
@@ -404,7 +404,7 @@ class PreferentialGPSampler(optuna.samplers.BaseSampler):
             is_all_discrete = all(
                 len(possible_params) > 0 for possible_params in all_possible_params.values()
             )
-            search_space_size = np.prod(
+            search_space_size = math.prod(  # Don't use np.prod to avoid overflow
                 [len(possible_params) for possible_params in all_possible_params.values()]
             )
             # TODO(contramundum53): Fix this arbitrarily chosen limit.
