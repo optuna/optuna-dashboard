69e47ee01947a7f41135135facf7c8ee4250ab3d
porink0424 <daichi2120@gmail.com>
2024-02-14 16:48:00 +0900

    Organize files

--- tslib/src/main.tsx
+++ tslib/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+if (root === null) throw new Error("Root should not be null");
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
