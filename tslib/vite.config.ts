b3d553eb0a753b1a78348a17ad0b59772081fc55
porink0424 <daichi2120@gmail.com>
2024-02-16 10:57:15 +0900

    Add SQLite3 WASM

--- tslib/vite.config.ts
+++ tslib/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Necessary to load the SQLite3 WASM file.
+  // See: https://github.com/sqlite/sqlite-wasm?tab=readme-ov-file#usage-with-vite
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@sqlite.org/sqlite-wasm"],
+  },
   plugins: [react()],
 });
