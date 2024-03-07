6f0f783dab88271d1de5dae6cebe90010a43fd1f
c-bata <contact@c-bata.link>
2024-03-08 01:41:41 +0900

    Migrate to Vite on standalone app

--- standalone_app/webpack.config.js
+++ standalone_app/webpack.config.js
@@ -1,101 +1,48 @@
 const webpack = require('webpack');
 const path = require('path');
+const CompressionPlugin = require("compression-webpack-plugin");
 
-const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-const isDev = mode === 'development';
-const PUBLIC_PATH = process.env.PUBLIC_PATH ?? '/public/';
-if (!PUBLIC_PATH.endsWith('/')) {
-    PUBLIC_PATH += '/';
-}
-
-const typeScriptLoader = process.env.TYPESCRIPT_LOADER === "esbuild-loader" ? {
-    test: /\.tsx?$/,
-    exclude: [/node_modules/],
-    loader: 'esbuild-loader',
-    options: {
-        loader: 'tsx',
-        tsconfigRaw: require('./tsconfig.json')
-    }
-} : {
-    test: /\.tsx?$/,
-    exclude: [/node_modules/],
-    loader: 'ts-loader',
-    options: {
-        configFile: __dirname + '/tsconfig.json',
-        transpileOnly: isDev,
-        happyPackMode: true
-    }
-}
-
-var config = [
-    {
-        mode,
-        experiments: {
-            syncWebAssembly: true,
-            asyncWebAssembly: true,
-        },
-        entry: [__dirname + '/src/browser_app_entry.tsx'],
-        output: {
-            path: __dirname + '/public/',
-            filename: 'bundle.js',
-            publicPath: PUBLIC_PATH
-        },
-        module: {
-            rules: [{ oneOf: [typeScriptLoader] }]
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js']
-        },
-        plugins: [
-            new webpack.DefinePlugin({'IS_VSCODE': JSON.stringify(false)})
-        ]
+module.exports = {
+    mode: "production",
+    devtool: 'source-map',
+    experiments: {
+        syncWebAssembly: true,
+        asyncWebAssembly: true,
     },
-    {
-        mode,
-        experiments: {
-            syncWebAssembly: true,
-            asyncWebAssembly: true,
-        },
-        entry: [__dirname + '/src/vscode_entry.tsx'],
-        output: {
-            path: path.resolve(__dirname, '../vscode/assets/'),
-            filename: 'bundle.js',
-            publicPath: '/'
-        },
-        module: {
-            rules: [
-                { oneOf: [typeScriptLoader] },
-                {
-                    test: /\.wasm$/,
-                    type: "asset/inline",
-                },
-            ]
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js']
-        },
-        plugins: [
-            new webpack.DefinePlugin({'IS_VSCODE': JSON.stringify(true)})
-        ]
-    },
-];
-
-if (isDev) {
-    config[0].devtool = 'source-map';
-    config[0].cache = {
+    cache: {
         type: 'filesystem',
         buildDependencies: {
             config: [__filename],
         }
-    }
-    console.log('= = = = = = = = = = = = = = = = = = =');
-    console.log('DEVELOPMENT BUILD');
-    console.log(process.env.TYPESCRIPT_LOADER === 'esbuild-loader' ? 'esbuild-loader' : 'ts-loader');
-    console.log('= = = = = = = = = = = = = = = = = = =');
-} else {
-    const CompressionPlugin = require("compression-webpack-plugin");
-    config[0].plugins.push(new CompressionPlugin())
-    config[1].plugins.push(new CompressionPlugin())
-}
-
-module.exports = config;
+    },
+    entry: [__dirname + '/src/vscode_entry.tsx'],
+    output: {
+        path: path.resolve(__dirname, '../vscode/assets/'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            { oneOf: [{
+                test: /\.tsx?$/,
+                exclude: [/node_modules/],
+                loader: 'esbuild-loader',
+                options: {
+                    loader: 'tsx',
+                    tsconfigRaw: require('./tsconfig.json')
+                }
+            }] },
+            {
+                test: /\.wasm$/,
+                type: "asset/inline",
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    plugins: [
+        new webpack.DefinePlugin({ 'IS_VSCODE': JSON.stringify(true) }),
+        new CompressionPlugin()
+    ]
+};
