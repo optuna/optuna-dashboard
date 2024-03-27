08016ddd609a06a0bfed818f5ee91e9bac5a4a09
porink0424 <daichi2120@gmail.com>
2024-03-27 13:38:25 +0900

    Resolve conflicts

---
+++ tslib/storybook/src/components/index.ts
@@ -0,0 +1,2 @@
+export { DataGrid } from "./DataGrid";
+export type { DataGridColumn } from "./DataGrid";
--- tslib/storybook/vite.config.ts
+++ tslib/storybook/vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,22 @@ export default defineConfig({
     exclude: ["@sqlite.org/sqlite-wasm"],
   },
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "@optuna/storybook",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    outDir: "pkg",
+    emptyOutDir: false,
+  }
 });
