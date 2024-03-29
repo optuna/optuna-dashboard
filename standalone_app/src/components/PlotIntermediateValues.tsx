02638e853a897b1b0b62ad748a8ed9c0ddfecb46
porink0424 <daichi2120@gmail.com>
2024-03-29 11:13:32 +0900

    Delete plotlyDarkTemplate from standalone_app

--- standalone_app/src/components/PlotIntermediateValues.tsx
+++ standalone_app/src/components/PlotIntermediateValues.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography, useTheme } from "@mui/material"
+import { plotlyDarkTemplate } from "@optuna/storybook"
 import * as plotly from "plotly.js-dist-min"
 import React, { FC, useEffect } from "react"
-import { plotlyDarkTemplate } from "../PlotlyDarkMode"
 
 const plotDomId = "graph-intermediate-values"
 
--- tslib/storybook/src/index.ts
+++ tslib/storybook/src/index.ts
@@ -2,3 +2,4 @@ export { DataGrid } from "./components/DataGrid";
 export type { DataGridColumn } from "./components/DataGrid";
 export { PlotHistory } from "./components/PlotHistory";
 export { TrialTable } from "./components/TrialTable";
+export { plotlyDarkTemplate } from "./components/PlotlyDarkMode";
