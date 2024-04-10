64777791f3b8964c8f7d1271e26767f35d06b96c
porink0424 <daichi2120@gmail.com>
2024-04-10 16:39:49 +0900

    Update darkTheme config

--- tslib/react/src/styles/darkTheme.ts
+++ tslib/react/src/styles/darkTheme.ts
@@ -1,7 +1,11 @@
 import { createTheme } from "@mui/material"
+import blue from "@mui/material/colors/blue"
+import pink from "@mui/material/colors/pink"
 
 export const darkTheme = createTheme({
   palette: {
     mode: "dark",
+    primary: blue,
+    secondary: pink,
   },
 })
