9b5ae543057062beb7a0ddd3fc06cd9d65a6462d
porink0424 <daichi2120@gmail.com>
2024-06-12 17:30:04 +0900

    Export PlotEdf & trialFilter from tslib/react

--- tslib/react/src/index.ts
+++ tslib/react/src/index.ts
@@ -1,9 +1,20 @@
 export { DataGrid } from "./components/DataGrid"
 export type { DataGridColumn } from "./components/DataGrid"
 export { plotlyDarkTemplate } from "./components/PlotlyDarkMode"
+export { PlotEdf, getPlotDomId } from "./components/PlotEdf"
+export type { EdfPlotInfo } from "./components/PlotEdf"
 export { PlotHistory } from "./components/PlotHistory"
 export { PlotImportance } from "./components/PlotImportance"
 export { PlotIntermediateValues } from "./components/PlotIntermediateValues"
 export { TrialTable } from "./components/TrialTable"
 export { GraphContainer } from "./components/GraphContainer"
 export { useGraphComponentState } from "./hooks/useGraphComponentState"
+export {
+  Target,
+  useFilteredTrials,
+  useFilteredTrialsFromStudies,
+  useObjectiveTargets,
+  useParamTargets,
+  useObjectiveAndUserAttrTargets,
+  useObjectiveAndUserAttrTargetsFromStudies,
+} from "./utils/trialFilter"
