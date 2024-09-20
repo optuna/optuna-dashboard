cb546a215788eab28f15048cac77c7e3230cf41f
keisuke-umezawa <keisuke.umezawa@gmail.com>
2024-09-20 14:20:12 +0900

    Add PlotTimeline.test.tsx

---
+++ tslib/react/test/PlotTimeline.test.tsx
@@ -0,0 +1,32 @@
+import * as Optuna from "@optuna/types"
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import { describe, expect, test } from "vitest"
+import { PlotTimeline } from "../src/components/PlotTimeline"
+
+describe("PlotSlice Tests", async () => {
+  const setup = ({
+    study,
+    dataTestId,
+  }: { study: Optuna.Study; dataTestId: string }) => {
+    const Wrapper = ({
+      dataTestId,
+      children,
+    }: {
+      dataTestId: string
+      children: React.ReactNode
+    }) => <div data-testid={dataTestId}>{children}</div>
+    return render(
+      <Wrapper dataTestId={dataTestId}>
+        <PlotTimeline study={study} />
+      </Wrapper>
+    )
+  }
+
+  for (const study of window.mockStudies) {
+    test(`PlotSlice (study name: ${study.name})`, () => {
+      setup({ study, dataTestId: `plot-slice-${study.id}` })
+      expect(screen.getByTestId(`plot-slice-${study.id}`)).toBeInTheDocument()
+    })
+  }
+})
