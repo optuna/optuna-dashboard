068ad1590ad44ef32d270eb3f845c0344dbcaee7
porink0424 <daichi5967@gmail.com>
2024-11-06 17:50:49 +0900

    Fix clear_inmemory_cache() function

--- e2e_tests/utils.py
+++ e2e_tests/utils.py
@@ -1,5 +1,7 @@
 from optuna_dashboard._storage import trials_cache
 from optuna_dashboard._storage import trials_cache_lock
+from optuna_dashboard._cached_extra_study_property import cached_extra_study_property_cache
+from optuna_dashboard._cached_extra_study_property import cached_extra_study_property_cache_lock
 from optuna_dashboard._storage import trials_last_fetched_at
 from playwright.sync_api import Page
 
@@ -8,6 +10,8 @@ def clear_inmemory_cache() -> None:
     with trials_cache_lock:
         trials_cache.clear()
         trials_last_fetched_at.clear()
+    with cached_extra_study_property_cache_lock:
+        cached_extra_study_property_cache.clear()
 
 
 def count_components(page: Page, component_name: str):
