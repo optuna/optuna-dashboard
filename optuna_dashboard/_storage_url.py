f200f84d3e0facceb4301e543b65bb82c2950063
msdsm <y.msd0110@gmail.com>
2025-05-31 10:50:49 +0900

    fix lint

--- optuna_dashboard/_storage_url.py
+++ optuna_dashboard/_storage_url.py
@@ -97,9 +97,9 @@ def get_journal_file_storage(file_path: str) -> JournalStorage:
     if version.parse(optuna_ver) < version.Version("v3.1.0"):
         raise ValueError("JournalRedisStorage is available from Optuna v3.1.0")
 
-    from optuna.storages.journal import JournalFileBackend
     from optuna.storages import JournalFileOpenLock
     from optuna.storages import JournalStorage
+    from optuna.storages.journal import JournalFileBackend
 
     storage: JournalStorage
     if os.name == "nt":
