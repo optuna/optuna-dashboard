c426fb71808c8b2a4b070ca69e98aa2cab6d825f
msdsm <y.msd0110@gmail.com>
2025-05-31 11:21:11 +0900

    fix flake8 error

--- optuna_dashboard/_storage_url.py
+++ optuna_dashboard/_storage_url.py
@@ -99,7 +99,7 @@ def get_journal_file_storage(file_path: str) -> JournalStorage:
 
     from optuna.storages import JournalFileOpenLock
     from optuna.storages import JournalStorage
-    
+
     try:
         from optuna.storages.journal import JournalFileBackend
     except ImportError:
