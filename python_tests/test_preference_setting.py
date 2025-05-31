eb11cc7fdc0e53e0f76390ae4744a09120f632b1
尾崎令拓 <ryota55ozaki@pfn-ryota-mbp2024.local>
2025-05-31 11:14:35 +0900

    fix system attrs in preferential opt

--- python_tests/test_preference_setting.py
+++ python_tests/test_preference_setting.py
@@ -12,7 +12,7 @@ class FeedbackSettingTestCase(TestCase):
     def test_widget_to_dict_from_dict(self) -> None:
         study = PreferentialStudy(optuna.create_study())
         register_preference_feedback_component(study, "artifact", "image_key")
-        system_attrs = study._study.system_attrs
+        system_attrs = study._study._storage.get_study_system_attrs(study._study._study_id)
         feedback_type = system_attrs.get(_SYSTEM_ATTR_FEEDBACK_COMPONENT, {})
         assert "output_type" in feedback_type
         assert feedback_type["output_type"] == "artifact"
