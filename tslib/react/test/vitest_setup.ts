fe0e20af7690685dcb866d5a08f4a7f5359e2d42
c-bata <contact@c-bata.link>
2025-07-18 18:10:40 +0900

    Refactor vitest_setup.ts

--- tslib/react/test/vitest_setup.ts
+++ tslib/react/test/vitest_setup.ts
@@ -8,20 +8,6 @@ afterEach(() => {
   cleanup()
 })
 
-// Ensure global cleanup of async operations
-afterEach(() => {
-  // Cancel any pending timers or intervals
-  if (typeof window !== "undefined") {
-    // Clear any pending timeouts/intervals
-    const highestTimeoutId = Number(setTimeout(() => {}))
-    clearTimeout(highestTimeoutId)
-    for (let i = 1; i < highestTimeoutId; i++) {
-      clearTimeout(i)
-      clearInterval(i)
-    }
-  }
-})
-
 declare global {
   namespace jest {
     interface Matchers<R> {
