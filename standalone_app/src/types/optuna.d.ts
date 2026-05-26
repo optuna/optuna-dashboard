a99cd38ed65651935ae71c8266d693d734e40440
c-bata <contact@c-bata.link>
2026-05-26 19:33:07 +0900

    Revert standalone_app/src/types/optuna.d.ts

--- standalone_app/src/types/optuna.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "optuna" {
-  export function wasm_fanova_calculate(
-    features: number[][],
-    targets: number[]
-  ): number[]
-
-  export default function init(module_or_path?: unknown): Promise<unknown>
-}
