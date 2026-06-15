2c0e5e050a97defcc3114ffc068031ab64fe32d1
c-bata <contact@c-bata.link>
2026-06-15 14:41:54 +0900

    Isolate JupyterLab extension React runtime

--- jupyterlab/src/widget.tsx
+++ jupyterlab/src/widget.tsx
@@ -1,14 +1,30 @@
-import { ReactWidget } from "@jupyterlab/ui-components"
+import { Message } from "@lumino/messaging"
+import { Widget } from "@lumino/widgets"
 import React from "react"
+import { createRoot, Root } from "react-dom/client"
 import { JupyterLabEntrypoint } from "./components/JupyterLabEntrypoint"
 
-export class OptunaDashboardWidget extends ReactWidget {
+export class OptunaDashboardWidget extends Widget {
+  private _root: Root | null = null
+
   constructor() {
     super()
     this.addClass("jp-react-widget")
   }
 
-  render(): React.ReactElement {
-    return <JupyterLabEntrypoint />
+  protected onAfterAttach(msg: Message): void {
+    super.onAfterAttach(msg)
+    if (this._root === null) {
+      this._root = createRoot(this.node)
+      this._root.render(<JupyterLabEntrypoint />)
+    }
+  }
+
+  dispose(): void {
+    if (this._root !== null) {
+      this._root.unmount()
+      this._root = null
+    }
+    super.dispose()
   }
 }
